ca7cb9effe2ab98a555440b6e9f78b33927f0f5a
Piotr01-K <pkazpraca@wp.pl>
2025-12-06 17:32:03 +0100

    Lesson 19 – Task 3: utworzenie  aplikacji ogloszenia

--- Django/Projekt_19/mojastrona/settings.py
+++ Django/Projekt_19/mojastrona/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ogloszenia',   # dodane w ramach zadania 3
 ]
 
 MIDDLEWARE = [
