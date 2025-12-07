88874a820f369dbb763327b683d7ad159cd166e3
Piotr01-K <pkazpraca@wp.pl>
2025-12-07 21:01:10 +0100

    Remove venv from repository

--- Django/Projekt_19/ogloszenia/models.py
+++ Django/Projekt_19/ogloszenia/models.py
@@ -8,3 +8,12 @@ class Ogloszenie(models.Model):
 
     def __str__(self):
         return self.tytul
+    
+#  dodałem w ramach Task 3
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
