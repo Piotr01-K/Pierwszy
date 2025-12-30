7d3690e58f3175ddb68e6296b2fe4f12a1804526
Piotr01-K <pkazpraca@wp.pl>
2025-12-30 12:17:12 +0100

    Lesson 23 Task 2: CarAdmin with list_display

--- Django/Projekt_19/cars/admin.py
+++ Django/Projekt_19/cars/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Car
 
-admin.site.register(Car)
+#   admin.site.register(Car)   #  zahashowane przy Lesson 23 task 2 (zapis zastąpiony lepszym)
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'is_available')
