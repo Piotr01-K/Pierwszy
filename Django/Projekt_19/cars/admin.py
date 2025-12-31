7e455f16c7f45035874bcd008f68c45b1313ba8a
Piotr01-K <pkazpraca@wp.pl>
2025-12-31 12:06:52 +0100

    Lesson 23 task 4: CarAdmin with list_filter

--- Django/Projekt_19/cars/admin.py
+++ Django/Projekt_19/cars/admin.py
@@ -6,4 +6,5 @@ from .models import Car
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'year', 'is_available')
-    search_fields = ('brand', 'model')
+    search_fields = ('brand', 'model')    # dodane w ramach Lesson 23 task 3
+    list_filter = ('is_available', 'year')    # dodane w ramach Lesson 23 task 4
