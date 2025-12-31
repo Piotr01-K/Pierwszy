cae315eeb0892cd6e04aa3531a852e6acc133b38
Piotr01-K <pkazpraca@wp.pl>
2025-12-31 15:11:18 +0100

    lesson 23 task 5: default ordering by year desc in admin

--- Django/Projekt_19/cars/admin.py
+++ Django/Projekt_19/cars/admin.py
@@ -8,3 +8,4 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'year', 'is_available')
     search_fields = ('brand', 'model')    # dodane w ramach Lesson 23 task 3
     list_filter = ('is_available', 'year')    # dodane w ramach Lesson 23 task 4
+    ordering = ('-year',)
