d15a5f177f5e28f2871927e7be37c48b5cf348ee
Piotr01-K <pkazpraca@wp.pl>
2026-01-17 11:51:28 +0100

    Lesson 25 task 1–2

--- Django/Projekt_19/products/serializers.py
+++ Django/Projekt_19/products/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
-
+#   Dodane w ramach task 2 Lesson 25
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
