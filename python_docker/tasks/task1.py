e726c87c7e535245d85a56d927b561fc0020d792
Piotr01-K <pkazpraca@wp.pl>
2026-03-01 00:07:21 +0100

    lesson 37 task 2: environment variable support

--- python_docker/tasks/task1.py
+++ python_docker/tasks/task1.py
@@ -1,3 +1,9 @@
-# task 1 hello_docker
+# task 1 hello_docker   (deleted in task 2)
 
-print("Hello, Docker!")
+# task 2 env_variable
+
+import os
+
+name = os.getenv("NAME", "World")
+
+print(f"Hello, {name}!")
